c05d98f450723c8f7d61ed41bf805e05e5bd2693
Michael Luo <michaelluo98@gmail.com>
2017-06-22 11:40:47 -0700

    Start users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(
+			:first_name, 
+			:last_name, 
+			:email, 
+			:password, 
+			:password_confirmation
+		)
+	end
+end
