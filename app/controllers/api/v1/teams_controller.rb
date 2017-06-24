90635fb07d42740973603162a9d44be0b875e5a3
Michael Luo <michaelluo98@gmail.com>
2017-06-24 13:39:17 -0700

    Add join team action

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -24,5 +24,21 @@ class Api::V1::TeamsController < Api::BaseController
 	end
 
 	def join
+		team = Team.find_by(id: params[:team_id])
+		game_mode = team.game.read_attribute_before_type_cast(:mode)
+		if (game_mode + 3) <= team.players.length 
+			render json: {
+				status: :failure, 
+				message: 'team has exceeded maximum length'
+			}
+		else 
+			team.players << current_user
+			render json: {
+				status: :success, 
+				message: 'successfully joined team', 
+				team: team, 
+				player: current_user
+			}
+		end
 	end
 end
