c8e879c08858c1450e386e6fb7390249802abd86
Michael Luo <michaelluo98@gmail.com>
2017-06-25 16:31:05 -0700

    Add sendmessage to directmessage

--- app/controllers/api/v1/directmessages_controller.rb
+++ app/controllers/api/v1/directmessages_controller.rb
@@ -1,14 +1,23 @@
 class Api::V1::DirectmessagesController < Api::BaseController
 	def sendmessage
 		recipient = User.find_by(id: params[:friend_id])
-		message = params[:message]
-		dm = Directmessage.new(sender: current_user, recipient: recipient)	
-	end
-
-	def receivemessage
+		message = params[:directmessage][:message]
+		dm = Directmessage.new(sender: current_user, recipient: recipient, message: message)	
+		if dm.save
+			render json: {
+				status: :success,
+				directmessage: dm 
+			}
+		else 
+			render json: {
+				status: :failure,
+				errors: dm.errors.full_messages.join('')
+			}
+		end
 	end
 
 	def index 
+
 	end
 
 	private
